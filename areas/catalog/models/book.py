4071a98a602d8f26188da8025d94218b204d2dd6
Stefano Teodorani <s.teodorani@gmail.com>
2014-08-24 17:44:57 +0200

    Added author

--- areas/catalog/models/book.py
+++ areas/catalog/models/book.py
@@ -13,6 +13,7 @@ class Book(db.Document):
     id = db.StringField(primary_key=True, default=str(uuid.uuid4()))
     isbn = db.StringField(required=True, max_length=40)
     title = db.StringField(max_length=200)
+    author = db.StringField(max_length=200)
     owner = db.StringField(required=True)
     status = db.IntField(required=True, choices=[1, 2, 3, 4, 5])
     notes = db.StringField(max_length=5000)
